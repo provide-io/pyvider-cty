be5e484b5c7398899dbc233b5bafc03d85906c92
Tim <code@tim.life>
2025-03-16 20:00:00 -0700

    refactor(cty): restructure base value handling for clarity and maintainability

--- src/pyvider/cty/values/base.py
+++ src/pyvider/cty/values/base.py
@@ -9,10 +9,10 @@ T = TypeVar('T', covariant=True)
 
 class CtyValue(Generic[T]):
     """Immutable representation of a Cty value."""
-    
-    def __init__(self, 
-                 type_: CtyType[T], 
-                 value: Any = None, 
+
+    def __init__(self,
+                 type_: CtyType[T],
+                 value: Any = None,
                  is_unknown: bool = False,
                  is_null: bool = False,
                  marks: Optional[FrozenSet] = None):
@@ -21,26 +21,26 @@ class CtyValue(Generic[T]):
         self._is_unknown = is_unknown
         self._is_null = is_null
         self._marks = marks or frozenset()
-    
+
     @property
     def type(self) -> CtyType[T]:
         """Get the type of this value."""
         return self._type
-    
+
     @property
     def is_known(self) -> bool:
         """Check if this value is known (not unknown)."""
         return not self._is_unknown
-    
+
     @property
     def is_null(self) -> bool:
         """Check if this value is null."""
         return self._is_null
-    
+
     def has_mark(self, mark: Any) -> bool:
         """Check if this value has a specific mark."""
         return mark in self._marks
-    
+
     def mark(self, mark: Any) -> "Value[T]":
         """Add a mark to this value."""
         return Value(
@@ -50,7 +50,7 @@ class CtyValue(Generic[T]):
             is_null=self._is_null,
             marks=frozenset(self._marks.union({mark}))
         )
-    
+
     def unmark(self) -> tuple["Value[T]", FrozenSet]:
         """Remove all marks from this value and return them."""
         return Value(
@@ -60,7 +60,7 @@ class CtyValue(Generic[T]):
             is_null=self._is_null,
             marks=frozenset()
         ), self._marks
-    
+
     def refine(self) -> "ValueRefinementBuilder":
         """Create a refinement builder for this value."""
         from pyvider.cty.values.refinement import ValueRefinementBuilder
@@ -86,3 +86,22 @@ class CtyValue(Generic[T]):
     def null(cls, type_: "CtyType") -> "CtyValue":
         """Create a null value of the given type."""
         return cls(type_=type_, is_null=True)
+
+    def __len__(self) -> int:
+        """Get the length of this value, similar to Go-CTY's behavior."""
+        # Cannot get length of unknown values
+        if self._is_unknown:
+            raise TypeError("Cannot get length of unknown value")
+
+        # Cannot get length of null values
+        if self._is_null:
+            raise TypeError("Cannot get length of null value")
+
+        # For known values, delegate to the underlying value
+        if hasattr(self._value, "__len__"):
+            return len(self._value)
+
+        # Value doesn't support length
+        raise TypeError(f"Value of type {type(self._value).__name__} doesn't support length operation")
+
+# 🐍🏗️
