114e0290dd29c346e6d1e03c1c8be6cb50a332c6
Tim <code@tim.life>
2025-06-16 20:00:00 -0700

    chore: update project configuration to resolve build failure

--- src/pyvider/cty/context/__init__.py
+++ src/pyvider/cty/context/__init__.py
@@ -1,4 +1,13 @@
 # pyvider/cty/context/__init__.py
-from .operation_context import OperationContext, get_current_operation
 
-__all__ = ["OperationContext", "get_current_operation"]
+from pyvider.cty.context.operation_context import (
+    OperationContext,
+    get_current_operation,
+    operation_context,
+)
+
+__all__ = [
+    "OperationContext",
+    "get_current_operation",
+    "operation_context",
+]
