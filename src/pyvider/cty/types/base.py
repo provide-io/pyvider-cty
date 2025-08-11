36db9ada972042aac5a324e76e2d0b0bc5b09b88
Tim <code@tim.life>
2025-08-11 13:30:24 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/pyvider/cty/types/base.py
+++ src/pyvider/cty/types/base.py
@@ -8,6 +8,7 @@ from typing import (
     TYPE_CHECKING,
     Any,
     ClassVar,
+    Generic,
     Protocol,
     TypeVar,
     runtime_checkable,
@@ -34,7 +35,7 @@ class CtyTypeProtocol(Protocol[T]):
 
 # The concrete ABC now implements the protocol
 @define(slots=True)
-class CtyType[T](CtyTypeProtocol[T], ABC):
+class CtyType(CtyTypeProtocol[T], ABC, Generic[T]):
     """
     Generic abstract base class for all Cty types.
     """
