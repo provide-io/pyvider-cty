0221998245af8bf809d8c61dc90b763ea9abd887
Tim <code@tim.life>
2025-11-28 20:00:00 -0800

    chore: update secrets configuration and dependencies

---
+++ .mutmut-config.py
@@ -0,0 +1,28 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""TODO: Add module docstring."""
+
+from typing import Any
+
+
+def pre_mutation(context: Any) -> None:
+    """
+    Called before each mutation is tested.
+    Can be used to skip certain mutations.
+    """
+    # Skip mutations in test files
+    if 'test_' in context.filename or '/tests/' in context.filename:
+        context.skip = True
+
+    # Skip mutations in generated files
+    if '_version.py' in context.filename:
+        context.skip = True
+
+    # Skip mutations in __init__ files (mostly imports)
+    if '__init__.py' in context.filename:
+        context.skip = True
+
+# 🌊🪢🔚
